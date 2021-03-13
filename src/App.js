281fa43532474aab67946bf4e52ac02d991852ab
crgarcia8 <crgarcia8@gmail.com>
2021-03-13 08:09:49 -0500

    Add cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Height from './Components/card';
+import {  Grid, makeStyles, Paper } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: 150
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <Paper className={classes.paper} elevation={5}></Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper} elevation={5}></Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper} elevation={5}></Paper>
+        </Grid><Grid item xs={3}>
+          <Paper className={classes.paper} elevation={5}></Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper} elevation={5}></Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper} elevation={5}></Paper>
+        </Grid><Grid item xs={3}>
+          <Paper className={classes.paper} elevation={5}></Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper} elevation={5}></Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper} elevation={5}></Paper>
+        </Grid><Grid item xs={3}>
+          <Paper className={classes.paper} elevation={5}></Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper} elevation={5}></Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper} elevation={5}></Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/Components/card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './card.scss';
+import Paper from '@material-ui/core/Paper';
+
+export default function Card() {
+
+  return (
+    <Paper elevation={5} />
+  );
+}
