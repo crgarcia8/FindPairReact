7ea15df3b3e4ec594f60b9b119f47b7100d8d492
crgarcia8 <crgarcia8@gmail.com>
2021-03-22 19:37:18 -0500

    fix

--- src/Constants.js
+++ src/Constants.js
@@ -3,12 +3,12 @@ export const _data = [
     { color: "yellow", source: "2", selected: false, persist: false },
     { color: "blue", source: "3", selected: false, persist: false },
     { color: "yellow", source: "4", selected: false, persist: false },
-    // { color: "yellow", source: "5", selected: false, persist: false },
-    // { color: "green", source: "6", selected: false, persist: false },
-    // { color: "gray", source: "7", selected: false, persist: false },
-    // { color: "orange", source: "8", selected: false, persist: false },
-    // { color: "pink", source: "9", selected: false, persist: false },
-    // { color: "pink", source: "10", selected: false, persist: false },
-    // { color: "green", source: "11", selected: false, persist: false },
-    // { color: "gray", source: "12", selected: false, persist: false }
+    { color: "yellow", source: "5", selected: false, persist: false },
+    { color: "green", source: "6", selected: false, persist: false },
+    { color: "gray", source: "7", selected: false, persist: false },
+    { color: "orange", source: "8", selected: false, persist: false },
+    { color: "pink", source: "9", selected: false, persist: false },
+    { color: "pink", source: "10", selected: false, persist: false },
+    { color: "green", source: "11", selected: false, persist: false },
+    { color: "gray", source: "12", selected: false, persist: false }
 ].sort((a, b) => Math.random() - 0.5);
